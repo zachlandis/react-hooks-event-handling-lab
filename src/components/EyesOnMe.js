6469bee30d4f7368b83ee26815e28ea810ab6cb4
Zach <zachlandis91@gmail.com>
2023-02-14 13:07:24 -0700

    commit tree 716b43f325e4ccec35190872dbc72caed902de60

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+function focusHandler() {
+    console.log('Good!')
+}
+
+function blurHandler() {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return (
+        <button 
+        onFocus={focusHandler} 
+        onBlur={blurHandler}>
+            Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
+function handleChange() {
+    console.log('Entering password...')
+}
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange}/>
+        </div>
     )
 }
 
